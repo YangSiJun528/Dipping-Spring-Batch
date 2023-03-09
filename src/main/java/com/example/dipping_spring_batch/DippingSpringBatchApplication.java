ae337ea9ffc34db91d43fb8f4577d8558820d7c1
YangSijun528 <yangsijun5528@gmail.com>
2023-03-09 16:03:56 +0900

    init

---
+++ src/main/java/com/example/dipping_spring_batch/DippingSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.dipping_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DippingSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DippingSpringBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dipping_spring_batch/DippingSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dipping_spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DippingSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
