7ae35452511a68323df8f18833f644e527de85e0
YangSijun528 <yangsijun5528@gmail.com>
2023-03-13 12:26:36 +0900

    update user domain add 'user_passed' column

--- src/main/java/com/example/dipping_spring_batch/test/domain/User.java
+++ src/main/java/com/example/dipping_spring_batch/test/domain/User.java
@@ -20,6 +20,9 @@ public class User {
     @Column(name = "user_name", nullable = false)
     String name;
 
+    @Column(name = "user_passed", nullable = true)
+    String passed;
+
     // scores는 연관관계의 주인(Score)의 user 필드에 해당한다.
     // cascade = ALL과 orphanRemoval = true 을 사용해 자식 엔티티의 생명주기를 관리한다.
     @OneToMany(mappedBy = "user", orphanRemoval = true, cascade = CascadeType.ALL)
