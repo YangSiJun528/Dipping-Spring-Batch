3ca9e233948f4a92f10c73dc9aa9efe3d7abc638
YangSijun528 <yangsijun5528@gmail.com>
2023-03-15 08:21:31 +0900

    정상 작동 확인
    와 이거 설마 @EnableBatchProcessing 하나 때문에 안됬던거임???

--- src/main/java/com/example/dipping_spring_batch/DippingSpringBatchApplication.java
+++ src/main/java/com/example/dipping_spring_batch/DippingSpringBatchApplication.java
@@ -1,16 +1,14 @@
 package com.example.dipping_spring_batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing
 @SpringBootApplication
 public class DippingSpringBatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DippingSpringBatchApplication.class, args);
-//        System.exit(SpringApplication.exit(SpringApplication.run(DippingSpringBatchApplication.class, args)));
+//        SpringApplication.run(DippingSpringBatchApplication.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(DippingSpringBatchApplication.class, args)));
     }
 
 }
