59420245969a28e7f3afb92a6ba2af4bba244f80
YangSijun528 <yangsijun5528@gmail.com>
2023-03-15 00:27:24 +0900

    TestTasklet 실행이 안됨;; 빈 등록만 된거같음

--- src/main/java/com/example/dipping_spring_batch/test/job/TestJobConfiguration.java
+++ src/main/java/com/example/dipping_spring_batch/test/job/TestJobConfiguration.java
@@ -5,11 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -48,8 +51,14 @@ public class TestJobConfiguration extends DefaultBatchConfiguration {
 
     @Bean
     public Tasklet testTasklet() {
-        log.warn("This is form TestTasklet");
-        return new TestTasklet();
+        Tasklet tasklet = new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                log.warn("This is form TestTasklet");
+                return RepeatStatus.FINISHED;
+            }
+        };
+        return tasklet;
     }
 
 }
